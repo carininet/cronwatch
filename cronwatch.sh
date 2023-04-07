99954e18cf445eafbe16958a81275e46c5fe6ce6
Alessandro Carini <alessandro.carini@technologist.com>
2023-04-07 11:37:29 +0200

    Add files via upload

--- cronwatch.sh
+++ cronwatch.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
 ## 
-## Notify system administrator that crontab has been running for too long
+## Notify system administrator that crontab or S3 backup has been running for too long
 ##
 
-# Script selfie
+
+# -= Functions =-
+
+# Convert a array to comma-separated string
+function a2s
+{
+	local IFS=","
+	a2sresult="${*}"
+}
+
+# Log and notify process identified by pid and parent
+notify()
+{
+	# Build pidlist and ppidlist as comma-separated string
+	allpids=(${pids[@]} ${ppids[@]})
+	a2s "${allpids[@]}"
+	allpidl=${a2sresult}
+	processes=$(ps --forest --format 'pid,ppid,user:10,tty,stat,pcpu,nlwp,time,pmem,rss:10,drs:10,size:10,lstart,cmd' --pid=${allpidl} --ppid=${allpidl})
+	message="${msg} pids=${allpidl}"$'\n'""$'\n'"${processes}"
+	logger -p local3.warning "${message}"
+	mail -s "${MYSELF}@${MYNODE} ${msg}" "${NOTIFY}" <<< "${message}"
+}
+
+# Selfie!
 SCRIPT=$(readlink -nf "${0}")
 HOMEDIR=$(dirname "${SCRIPT}")
 MYSELF=$(basename "${SCRIPT}" ".${SCRIPT##*.}")
@@ -12,36 +35,48 @@ MYNODE=$(hostname -f)
 
 # Constants
 PSREX='([0-9]+)\s+([0-9]+)\s+(\w+\s+\w+\s+[0-9]+\s+[0-9]+:[0-9]+:[0-9]+\s+[0-9]+)\s+(.+)'
-TIMEOUT=180
-SENDTO='sysadmin@example.com'
+CRONTIMEOUT=240
+NOTIFY='mymail@example.com'
 
-# Get cron processes
+# Get scheduler processes
 res=0
+
 npid=0
-cron=$(ps -o pid,ppid,lstart,cmd -C 'cron')
+pids=()
+ppids=()
+lstarts=()
+cmds=()
+pslist=$(ps --no-headers --format 'pid,ppid,lstart,cmd' -C 'cron')
 while read process; do
 	if [[ ${process} =~ ${PSREX} ]]; then
-		pid[$npid]="${BASH_REMATCH[1]}"
-		ppid[$npid]="${BASH_REMATCH[2]}"
-		lstart[$npid]="${BASH_REMATCH[3]}"
-		cmd[$npid]="${BASH_REMATCH[4]}"
+		pid="${BASH_REMATCH[1]}"
+		ppid="${BASH_REMATCH[2]}"
+		lstart="${BASH_REMATCH[3]}"
+		cmd="${BASH_REMATCH[4]}"
 
-		# Exclude main crontab process
-		if [[ ${ppid[$npid]} > 1 ]]; then
+		# Exclude process with parent = root
+		if [[ ${ppid} > 1 ]]; then
 			# Get process duration (minutes)
-			running=$(($(($(date +%s) - $(date -d "${lstart[$npid]}" +%s))) / 60))
-			if [[ $running > $TIMEOUT ]]; then
-				# Log to rsyslog and send warning mail
-				subject="${MYNODE} crontab is running for ${running} minutes"
-				message="${MYSELF}@${MYNODE}: Found stuck crontab pid=${pid[$npid]}, ppid=${ppid[$npid]}"
-				logger -p local3.warning "${message}"
-				mail -s "${subject}" "${SENDTO}" <<< "${message}"
+			running=$(($(($(date +%s) - $(date -d "${lstart}" +%s))) / 60))
+			if [[ $running > $CRONTIMEOUT ]]; then
+				pids[$npid]="${pid}"
+				ppids[$npid]="${ppid}"
+				lstarts[$npid]="${lstart}"
+				cmds[$npid]="${cmd}"
+				npid=$((npid+1))
 				res=1
 			fi
 		fi
-
-		npid=$((npid+1))
 	fi
-done <<< "${cron}"
+
+done <<< "${pslist}"
+
+# Report processes runnig longer than $CRONTIMEOUT
+if [[ ${npid} > 0 ]]; then
+	# Log to rsyslog and send warning mail
+	msg='Stuck crontab'
+	notify
+fi
 
 exit $res
+
